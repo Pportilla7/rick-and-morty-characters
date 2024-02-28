08c007bef5c59c74241d2155b2a4dad8cb380e96
Pablo Portilla Rivero <pportilla@rfe.es>
2024-02-28 12:26:25 +0100

    Se acaba por resolver el ejercicio. Se intenta poner de manera mas clara y sencilla con la utilizacion de funciones. Se da algo de estilo

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 let lista=document.getElementById('character-list');
 let page=1;
-let url='https://rickandmortyapi.com/api/character/?page='
-let objeto=fetch('https://rickandmortyapi.com/api/character/?page=1').then((response)=>{
-    if(!response.ok){
-        throw new Error('La solicitud NOK');
-    }
-    return response.json();
-})
-.then((data)=>{
-    data.results.forEach(element => {
-        console.log(element);
-        let elemento_lista = document.createElement('li');
-        let div_contenedor = document.createElement('div');
-        let imagen = document.createElement('img');
-        imagen.src = element.image;
-        let parrafo1 = document.createElement('p');
-        parrafo1.innerHTML = 'Nombre: '+element.name;
-        let parrafo2 = document.createElement('p');
-        parrafo2.innerHTML = 'Especie: '+element.species;
-        div_contenedor.appendChild(imagen);
-        div_contenedor.appendChild(parrafo1);
-        div_contenedor.appendChild(parrafo2);
-        elemento_lista.appendChild(div_contenedor);
-        lista.appendChild(elemento_lista);
-    });
-    return data;
-})
+const url='https://rickandmortyapi.com/api/character/?page='
+
+getPersonajes(url+page);
 
 let button_next=document.getElementById('next-page');
+let button_previous=document.getElementById('prev-page');
 
 button_next.addEventListener('click',()=>{
     lista.innerHTML='';
     page++;
     let url_def;
+    
+    //Si pasamos de la pagina 42 volvemos a la primera
     (page>42)?page=1:page=page;
     url_def=url+page;
-    console.log(url_def);
+    
+    getPersonajes(url_def);
+})  
+
+button_previous.addEventListener('click',()=>{
+    lista.innerHTML='';
+    page--;
+    let url_def;
+    console.log(page);
+    //Si bajamos de la pagina 1 pasamos a la 42
+    (page<1)?page=42:page=page;
+    url_def=url+page;
+    console.log(page);
+
+    getPersonajes(url_def);
+})  
+
+
+function getPersonajes(url_def){
     fetch(url_def).then((response)=>{
     if(!response.ok){
         throw new Error('La solicitud NOK');
@@ -43,23 +41,46 @@ button_next.addEventListener('click',()=>{
     return response.json();
     })
     .then((data)=>{
+        let aux=0;
         data.results.forEach(element => {
-        console.log(element);
-        let elemento_lista = document.createElement('li');
-        let div_contenedor = document.createElement('div');
-        let imagen = document.createElement('img');
-        imagen.src = element.image;
-        let parrafo1 = document.createElement('p');
-        parrafo1.innerHTML = 'Nombre: '+element.name;
-        let parrafo2 = document.createElement('p');
-        parrafo2.innerHTML = 'Especie: '+element.species;
-        div_contenedor.appendChild(imagen);
-        div_contenedor.appendChild(parrafo1);
-        div_contenedor.appendChild(parrafo2);
-        elemento_lista.appendChild(div_contenedor);
-        lista.appendChild(elemento_lista);
+            ContenedorLi(element,aux);
+            aux++;
         });
+    return data;
     })
-})  
+    .catch((error) => {
+        console.error('Error al obtener los personajes:', error);
+    });
+}
 
+function ContenedorLi(element,aux){
+    crearContenedor(aux);
+    
+    darValor(element,aux);
+    
+}
 
+function crearContenedor(aux){
+    let elemento_lista = document.createElement('li');
+    let imagen = document.createElement('img');
+    let parrafo1 = document.createElement('p');
+    let parrafo2 = document.createElement('p');
+
+    elemento_lista.id='Li_'+page+aux;
+    imagen.id='Img_'+page+aux;
+    parrafo1.id='P1_'+page+aux;
+    parrafo2.id='P2_'+page+aux;
+
+    elemento_lista.appendChild(imagen);
+    imagen.insertAdjacentElement('afterend', parrafo1);
+    parrafo1.insertAdjacentElement('afterend', parrafo2);
+    
+    lista.appendChild(elemento_lista);
+}
+
+function darValor(element,aux){
+    let cadena_aux='_'+page+aux;
+    (document.getElementById('Img'+cadena_aux)).src=element.image;
+    (document.getElementById('P1'+cadena_aux)).textContent='Name: '+element.name;
+    (document.getElementById('P2'+cadena_aux)).textContent='Specie: '+element.species;
+}
